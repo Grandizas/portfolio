0a7ed493416092f665f553a644fc47e111eb0873
Grazvydas Z <grazvydaszilius2001@gmail.com>
2024-07-27 08:24:46 +0300

    New: Added loading message, for spotify list.

--- components/templates/Menu.vue
+++ components/templates/Menu.vue
@@ -66,16 +66,18 @@
       </h3>
 
       <!-- * --- Spotify list --- * -->
-      <iframe
-        style="border-radius: 12px"
-        src="https://open.spotify.com/embed/playlist/5VinPcigPXUER5zK6bRTyK?utm_source=generator&theme=0"
-        width="100%"
-        height="352"
-        frameBorder="0"
-        allowfullscreen="false"
-        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-        loading="lazy"
-      ></iframe>
+      <div class="spotify-list">
+        <iframe
+          style="border-radius: 12px"
+          src="https://open.spotify.com/embed/playlist/5VinPcigPXUER5zK6bRTyK?utm_source=generator&theme=0"
+          width="100%"
+          height="352"
+          frameBorder="0"
+          allowfullscreen="false"
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+          loading="lazy"
+        ></iframe>
+      </div>
     </div>
   </div>
 </template>
