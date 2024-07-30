d083d5716ff75f7ae033e3290613a466c1da8ff7
Grazvydas Z <grazvydaszilius2001@gmail.com>
2024-07-30 12:19:51 +0300

    New: Added error page.

---
+++ layouts/error.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Page Not Found</h1>
+    <p>Redirecting to the home page...</p>
+  </div>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+// Redirect to the root path after 2 seconds
+setTimeout(() => {
+  router.push('/');
+}, 2000);
+</script>
